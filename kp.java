da625b64042815e5e8a7cb34c0631568d818ede2
dk <dk.p@gmail.com>
2020-03-29 14:04:33 -0400

    secound commit

---
+++ kp.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+       
+    int a=2;
+    int b=3;
+    int c=5;
+    int rectPER = (a*b) + (a*c);
+    System.out.println ("Perimeter is:" + rectPER);
+ 
+    int diameter;
+    int radius=4;
+    diameter= 2*radius ;
+    System.out.println ("Diameter is:" + diameter);
+}
